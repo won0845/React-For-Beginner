bccd54737798457c9c8dd117e314f4c3ceea87c6
LEE <tpqms775@gmail.com>
2024-04-10 01:24:49 +0900

    refactor: 버전에 맞게 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie/:id"  element={<Detail/>}/>
+        <Route path="/"  element={<Home />}/>
+      </Routes>
+    </Router>
+  )
 }
 export default App;
