913fd6bcb22cefe4e5a013bb550ff3040ef461d4
LEE <tpqms775@gmail.com>
2024-04-10 13:14:22 +0900

    feat: 영화 상세정보 페이지

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,32 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 function Detail() {
-  return (
-      <h1>Detail</h1>
-  );
+    const [response, setResponse] = useState(null);
+    const { id } = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`))
+            .json();
+        setResponse(json);
+        console.log(json.data.movie,1)
+    };
+    useEffect(() => {
+        getMovie();
+    }, [])
+    if (!response) return <h1>Loading...</h1>
+    const movie = response.data.movie;
+    return (
+        <div key={movie.id}>
+            <img src={movie.large_cover_image} alt={movie.title} />
+            <h2>
+                {movie.title}
+            </h2>
+            <p>{movie.description_full}</p>
+            <ul>
+                {movie.genres.map((g) => <li key={g}>{g}</li>)}
+            </ul>
+        </div>
+   );
 }
 export default Detail;
